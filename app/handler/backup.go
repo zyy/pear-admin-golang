51b7f768eb62c646fc88f5a3729460b664ff76ab
hahaxi <cilidm@foxmail.com>
2022-06-16 17:48:06 +0800

    修改文件夹命名

---
+++ app/handler/backup.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"net/http"
+	"pear-admin-go/app/global/request"
+	"pear-admin-go/app/global/response"
+	"pear-admin-go/app/service"
+	"pear-admin-go/app/util/validate"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TaskList(c *gin.Context) {
+	c.HTML(http.StatusOK, "task_list.html", nil)
+}
+
+func TaskJson(c *gin.Context) {
+	var f request.TaskPage
+	if err := c.ShouldBind(&f); err != nil {
+		response.ErrorResp(c).SetMsg(validate.GetValidateError(err)).WriteJsonExit()
+		return
+	}
+	data, count, err := service.TaskJson(f)
+	if err != nil {
+		response.SuccessResp(c).SetCode(0).SetMsg(err.Error()).SetCount(count).WriteJsonExit()
+		return
+	}
+	response.SuccessResp(c).SetCode(0).SetCount(count).SetData(data).WriteJsonExit()
+}
+
+func TaskAdd(c *gin.Context) {
+	if c.Request.Method == "GET" {
+		servers, _, _ := service.ServerList()
+		c.HTML(http.StatusOK, "task_add.html", gin.H{"servers": servers})
+	} else {
+		// response.ErrorResp(c).SetMsg("演示服务器暂不提供此功能，请下载源码或发行版使用").WriteJsonExit()
+		// return
+		var f request.TaskForm
+		if err := c.ShouldBind(&f); err != nil {
+			response.ErrorResp(c).SetMsg(validate.GetValidateError(err)).WriteJsonExit()
+			return
+		}
+		err := service.TaskAdd(f)
+		if err != nil {
+			response.ErrorResp(c).SetMsg(err.Error()).WriteJsonExit()
+			return
+		}
+		response.SuccessResp(c).SetMsg("任务创建成功，已在后台运行，请稍后查看文件数量!").WriteJsonExit()
+		return
+	}
+}
+
+func TaskEdit(c *gin.Context) {
+	c.HTML(http.StatusOK, "task_edit.html", nil)
+}
+
+func TaskDel(c *gin.Context) {
+
+}
--- app/router/task.go
+++ app/router/task.go
@@ -2,24 +2,23 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	"pear-admin-go/app/controller"
 )
 
 func TaskRouter(r *gin.Engine) {
 	tr := r.Group("system")
-	tr.GET("server/list", controller.ServerList)
-	tr.GET("server/json", controller.ServerJson)
-	tr.GET("server/add", controller.ServerAdd)
-	tr.POST("server/add", controller.ServerAdd)
-	tr.GET("server/edit", controller.ServerEdit)
-	tr.POST("server/edit", controller.ServerEdit)
-	tr.POST("server/del", controller.ServerDel)
+	tr.GET("server/list", handler.ServerList)
+	tr.GET("server/json", handler.ServerJson)
+	tr.GET("server/add", handler.ServerAdd)
+	tr.POST("server/add", handler.ServerAdd)
+	tr.GET("server/edit", handler.ServerEdit)
+	tr.POST("server/edit", handler.ServerEdit)
+	tr.POST("server/del", handler.ServerDel)
 
-	tr.GET("task/list", controller.TaskList)
-	tr.GET("task/json", controller.TaskJson)
-	tr.GET("task/add", controller.TaskAdd)
-	tr.POST("task/add", controller.TaskAdd)
-	tr.GET("task/edit", controller.TaskEdit)
-	tr.POST("task/edit", controller.TaskEdit)
-	tr.POST("task/del", controller.TaskDel)
+	tr.GET("task/list", handler.TaskList)
+	tr.GET("task/json", handler.TaskJson)
+	tr.GET("task/add", handler.TaskAdd)
+	tr.POST("task/add", handler.TaskAdd)
+	tr.GET("task/edit", handler.TaskEdit)
+	tr.POST("task/edit", handler.TaskEdit)
+	tr.POST("task/del", handler.TaskDel)
 }
